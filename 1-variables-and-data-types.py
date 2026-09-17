6f6d3ebdda8ff1ce55198feeba9f93925d236427
“Sazu97” <sara97rm@gmail.com>
2026-09-17 13:27:34 +0200

    feat: complete exercise 3 or variables

--- 1-variables-and-data-types.py
+++ 1-variables-and-data-types.py
@@ -22,11 +22,27 @@ Escribe en un comentario de línea lo que sucede.
 mensaje = "Hello, world!"
 print(mensaje)
 # El valor anterior se sobrescribe y ahora la variable contiene el nuevo texto.
+
 """
 --- Ejercicio 3 Tipos de datos---
 Crea variables para cada uno de los siguientes tipos de datos y colecciones: string, int, float, 
 bool, list, tuple, dicctionary and set. 
 Imprime cada variable y el tipo de dato o colección que almacena en la consola.
 """
-# Escribe tu código aquí
+my_string = "Potato"
+my_int = 28
+my_float = 8.88
+my_bool = True
+my_list = [2,3,4,5]
+my_tuple = (6,7,8)
+my_dicctionary = {"nombre": "Sara", "edad": 28}
+my_set = {1,2,3,4}
 
+print(my_string, type(my_string))
+print(my_int, type(my_int))
+print(my_float, type(my_float))
+print(my_bool, type(my_bool))
+print(my_list, type(my_list))
+print(my_tuple, type(my_tuple))
+print(my_dicctionary, type(my_dicctionary))
+print(my_set, type(my_set))
